eb1e6cd8eb75dc363a2d7dbac06d8e303a45b1c9
obi william <williamobi818@gmail.com>
2023-03-17 00:01:40 +0000

    work

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
- * main - main entry point
- * return : always 0 (success)
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
-int main(void)
+int main(0)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
