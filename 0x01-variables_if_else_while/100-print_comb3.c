b1b9924f79627a0b6ff16354a7fa54cdab3a697e
obi william <williamobi818@gmail.com>
2023-03-18 17:14:54 +0000

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -17,11 +17,13 @@ int main(void)
 			{
 				putchar(i + '0');
 				putchar(j + '0');
+				if (i == 8 && j == 9)
+					putchar('\n');
+				else
 				putchar(',');
 				putchar(' ');
 			}
 	}
 	}
-	putchar('\n');
 		return (0);
 }
