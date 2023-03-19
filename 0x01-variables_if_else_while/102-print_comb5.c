cfaa598ab11f1ed553f868b4562c5d7142b5f0cd
obi william <williamobi818@gmail.com>
2023-03-19 22:19:57 +0000

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - main
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 00; i < 100; i++)
+	{
+		for (j = 00; j < 100; j++)
+		{
+			if (i < j && i != j)
+			{
+				putchar(i / 10 + '0');
+				putchar(i % 10 + '0');
+				putchar(' ');
+				putchar(j / 10 + '0');
+				putchar(j % 10 + '0');
+				if (i + j != 197)
+				{
+					putchar(',');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
