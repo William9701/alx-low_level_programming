ba660b201002a905e6dcff7e4de85ab40dff2759
obi william <williamobi818@gmail.com>
2023-03-20 01:32:23 +0000

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,20 @@
+#include "main.h"
 #include <stdio.h>
-
 /**
  * main - main
  * Return: always 0
  */
+
+/*aphabet prin*/
 void print_alphabet(void)
 {
 	char c;
+
 	for (c = 'a'; c <= 'z'; c++)
 	{
 		putchar(c);
 	}
 }
-
 int main(void)
 {
 	print_alphabet();
