5e3961bb3824c7fba602fd9bf7c62c3fd498b286
obi william <williamobi818@gmail.com>
2023-03-21 19:01:27 +0000

    work

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - make aphlabets 10xtimes
+ * Return: always o
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int i = 0;
+	while (i <= 0)
+	{
+		for (c = 'a'; c <= 'z'; c++)		
+		{
+			_putchar(c);
+		}
+	_putchar('\n');
+	i++;
+	}
+}
