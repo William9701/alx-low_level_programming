d096a4bddb9a0bfde7d2248c14a365ccdaaeaa11
obi william <williamobi818@gmail.com>
2023-03-22 07:46:39 +0000

    still_working

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,8 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- * print_numbers - print numbers from 0 to 9
+ * print_numbers - print numbers
  * Return: alwys return 0
  */
 void print_numbers(void)
@@ -10,8 +11,7 @@ void print_numbers(void)
 
 	for (c = 0; c < 10; c++)
 	{
-		_putchar(c + '0');
-		c++;
+		putchar(c + '0');
 	}
-	_putchar('\n');
+	putchar('\n');
 }
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_most_numbers - print only a few numbers
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i != 2 && i != 4)
+		{
+			putchar(i + '0');
+		}
+	}
+	putchar('\n');
+}
