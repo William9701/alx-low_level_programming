8c9b32f590be024de442a755729fc8603df21b47
obi william <williamobi818@gmail.com>
2023-03-27 11:40:05 +0000

    new_day

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - reset_to_98
+ * @n: charcter to be worked with
+ * Return: nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,4 @@
+# This is a c-string, pionter and arries repo
+
+created by obi william
+feel free to clone and fork this repo
