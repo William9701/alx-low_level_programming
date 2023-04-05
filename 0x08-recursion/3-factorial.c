e785f1782ac296e83f916648f2f554b9c7b5180e
obi william <williamobi818@gmail.com>
2023-04-05 18:07:36 +0000

    update

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -2,7 +2,7 @@
 
 /**
  * factorial - factorial
- * @n: input 
+ * @n: input
  * Return: 0
  */
 
@@ -11,7 +11,7 @@ int factorial(int n)
 	if (n < 0)
 		return (-1);
 	if (n == 0)
-		return (1);	
-	else
+		return (1);
+
 	return (n * factorial(n - 1));
 }
