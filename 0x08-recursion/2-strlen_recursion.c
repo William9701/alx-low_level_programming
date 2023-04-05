c2174b705ff0e90106c71c6b2488958701bdc583
obi william <williamobi818@gmail.com>
2023-04-05 15:56:34 +0000

    work

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -10,9 +10,8 @@ int _strlen_recursion(char *s)
 {
 
 	if (*s == '\0')
-		return 0;
+		return (0);
 	if (*s >= 'a' || *s <= 'z')
-		return 1 + _strlen_recursion(s + 1);
-	else
-	return _strlen_recursion(s + 1);
+		return (1 + _strlen_recursion(s + 1));
+	return (_strlen_recursion(s + 1));
 }
