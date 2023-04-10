29be353ea16b14d4f961b3f59481df129042fd82
obi william <williamobi818@gmail.com>
2023-04-10 11:30:24 +0000

    arg

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - main 
+ * @argc: input
+ * @argv: input
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+
+		printf("%d\n",argc-1);
+		return (0);
+
+		if (argc < 0)
+		{
+		int i;
+
+		for (i = 0; i < argc; i++)
+			printf("%s", argv[i]);
+		return (0);
+		}
+}
