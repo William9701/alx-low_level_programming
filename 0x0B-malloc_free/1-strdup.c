ced8d1353284c45ae6c06c78a0f0fd3c2ed47fdc
obi william <williamobi818@gmail.com>
2023-04-11 23:17:19 +0000

    _strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strdup - _strdup
+ * @str: input
+ * Return: ptr
+ */
+
+char *_strdup(char *str)
+{
+	int i;
+	int lenght = strlen(str);
+	char *ptr;
+
+	if (lenght == 0)
+	{
+		return (NULL);
+	}
+	ptr = (char *)malloc(lenght * sizeof(char));
+
+	for (i = 0; i < lenght; i++)
+	ptr[i] = str[i];
+
+return (ptr);
+}
