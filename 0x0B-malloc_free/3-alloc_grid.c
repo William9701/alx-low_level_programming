86f68825ba1cc73abd19787aa1878ece0f46da64
obi william <williamobi818@gmail.com>
2023-04-12 03:03:07 +0000

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -12,21 +12,35 @@
 int **alloc_grid(int width, int height)
 {
 	int **ptr;
-	int i;
+	int i, j;
 	if (width <= 0 || height <= 0)
 	{
 		return (NULL);
 	}
 	
-	ptr = malloc(sizeof(int*) * width);
+	ptr = malloc(sizeof(int*) * height);
 	if (ptr == 0)
 	{
 		return (NULL);
 	}
 
 	for (i = 0; i < height; i++)
+	{
 		ptr[i] = malloc(sizeof(int) * width);
-	ptr[i] = 0;
+	if (ptr[i] == 0)
+	{
+		for (; i >= 0; i--)
+			free(ptr[i]);
+		free(ptr);
+		return (NULL);
+	}
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+			ptr[i][j] = 0;
+	}
 	
 	return (ptr);
 }
