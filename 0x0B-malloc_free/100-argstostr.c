bcdb0c3aadde0be6707c208341b9faf6a94f7504
obi william <williamobi818@gmail.com>
2023-04-12 15:52:11 +0000

    a try

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -18,7 +18,7 @@ char *argstostr(int ac, char **av)
 	{
 		return (NULL);
 	}
-	ptr = malloc(sizeof(char) * (ac + 1));
+	ptr = malloc(sizeof(char) * (ac + 51));
 	if (ptr == 0)
 	{
 		return (NULL);
