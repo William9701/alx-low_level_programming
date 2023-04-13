5135af1027523537b4ad999e5f03eee2bbf451ef
obi william <williamobi818@gmail.com>
2023-04-13 20:51:39 +0000

    update

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,7 +1,25 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+char *_memset(char *c, char b, unsigned int n);
+/**
+ * _memset - _memset
+ * @c: input
+ * @b: input
+ * @n: input
+ * Return: pointr
+ */
 
+char *_memset(char *c, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		c[i] = b;
+	}
+	return (c);
+}
 /**
  * _calloc - _calloc
  * @nmemb: input
@@ -24,5 +42,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	{
 		return (NULL);
 	}
+	_memset(ptr, 0, nmemb * size);
+
 	return (ptr);
 }
