c202fb4968e4f59912c3df34efc142dd2ee0b712
obi william <williamobi818@gmail.com>
2023-04-14 12:54:34 +0000

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -17,7 +17,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (ptr == NULL)
 	{
-		ptr1 = malloc(new_size);
+		ptr1 = malloc(new_size * (sizeof(int)));
 	}
 	if (new_size == old_size)
 	{
@@ -31,7 +31,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	
 	j = new_size > old_size ? new_size : old_size;
 
-	ptr1 = malloc(new_size);
+	ptr1 = malloc(new_size * (sizeof(int)));
 	if (ptr1 == 0)
 	{
 		return (NULL);
