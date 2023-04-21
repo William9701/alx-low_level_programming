d273baad3fad62fa65c191a649acdb4e6303b70b
Obi william <williamobi818@gmail.com>
2023-04-21 00:46:06 +0000

    work

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -17,12 +17,6 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_list list;
 	char *str;
 
-	if (separator == NULL)
-	{
-		return;
-	}
-
-
 	va_start(list, n);
 
 	for (i = 0; i < n; i++)
