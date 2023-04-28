e27010f8e8d2e4680208130548c8cd1b66f130d3
Obi <williamobi818@gmail.com>
2023-04-28 01:49:52 +0000

    na wa oh

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,10 @@
+	global main
+	extern printf
+
+main:
+	mov edi, msg	; the printf msg is the first argument to be passed
+	xor eax, eax	; clear eax
+	call printf	; call printf
+	mov	 eax, 0	; clear eax
+	ret		;return
+msg:db `Hello,Holberton\n`,0
