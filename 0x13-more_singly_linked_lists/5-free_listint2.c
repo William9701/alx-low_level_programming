d879ca315f32f8158c21cbb460eb4a24f5dd171d
Obi <williamobi818@gmail.com>
2023-04-30 14:35:32 +0000

    work

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -12,6 +12,8 @@ void free_listint2(listint_t **head)
 	listint_t *ptr;
 	listint_t *next;
 
+	ptr = malloc(sizeof(listint_t));
+	next = malloc(sizeof(listint_t));
 	ptr = *head;
 	while (ptr != NULL)
 	{
