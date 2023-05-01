f18abd3cae5760689dc115fe0a06759bd4aa2138
Obi <williamobi818@gmail.com>
2023-05-01 08:03:57 +0000

    work

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,7 +15,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *ptr, *p;
 
 	ptr = malloc(sizeof(listint_t));
-	if (!ptr || *head)
+	if (!ptr ||!head)
 	{
 		return (NULL);
 	}
