0507fe6e373d48bb02b9e35ad9f71658bc1fc393
Obi <williamobi818@gmail.com>
2023-05-04 18:15:05 +0000

    work

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_binary - print_binary
+ * @n: n
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int m = 1UL << (sizeof(unsigned long int) * 7);
+	unsigned long int i;
+
+	if (n == 0)
+	{
+		printf("0");
+	}
+
+	while ((m & n) == 0 && m != 0)
+	{
+		m >>= 1;
+	}
+
+	for (i = 0; i < sizeof(unsigned long int) * 8 && m != 0; i++)
+	{
+		if ((m & n) != 0)
+		{
+			printf("1");
+		}
+		else
+		{
+			printf("0");
+		}
+		m >>= 1;
+	}
+}
