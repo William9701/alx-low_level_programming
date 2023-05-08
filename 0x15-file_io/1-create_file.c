ce16698cad27e88f2e633aad8813049e01778387
Obi <williamobi818@gmail.com>
2023-05-08 21:09:09 +0000

    work

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,10 +22,6 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	if (text_content == NULL)
-	{
-		text_content = "";
-	}
 	fp = open(filename,  O_WRONLY | O_CREAT | S_IWUSR | O_TRUNC, S_IRUSR);
 	if (fp == -1)
 	{
