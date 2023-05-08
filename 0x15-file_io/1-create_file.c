c770275194dd0d70a6f56754999fdb4f9ac020a3
Obi <williamobi818@gmail.com>
2023-05-08 20:58:58 +0000

    work

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -6,7 +6,7 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
-
+#include <string.h>
 /**
  * create_file - create_file
  * @filename: filename
@@ -16,6 +16,7 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fp;
+	ssize_t nwritten;
 
 	if (filename == NULL)
 	{
@@ -25,11 +26,19 @@ int create_file(const char *filename, char *text_content)
 	{
 		text_content = "";
 	}
-	fp = open(filename,  O_WRONLY | O_CREAT | O_EXCL, S_IRUSR |
-	S_IWUSR | O_TRUNC, S_IRUSR);
+	fp = open(filename,  O_WRONLY | O_CREAT | S_IWUSR | O_TRUNC, S_IRUSR);
 	if (fp == -1)
 	{
 		return (-1);
+	}
+	if (text_content != NULL)
+	{
+		nwritten = write(fp, text_content, strlen(text_content));
+		if (nwritten == -1)
+		{
+			close(fp);
+			return (-1);
+		}
 	}
 	close(fp);
 
