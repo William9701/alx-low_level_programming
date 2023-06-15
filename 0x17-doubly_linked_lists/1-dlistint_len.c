c62d14f46ab6a6c35674352e25c7b354191938ce
Obi <williamobi818@gmail.com>
2023-06-15 13:26:50 +0000

    work

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * dlistint_len - list_len
+ * @h: input
+ * Return: count
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h != 0)
+	{
+	if (h->n == 0)
+	{
+		printf("[0] (nil)\n");
+	}
+	else
+	{
+		count++;
+		h = h->next;
+	}
+	}
+	return (count);
+}
