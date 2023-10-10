81ea47289cb45e94db96350886e3b3b23b274984
Obi <williamobi818@gmail.com>
2023-10-10 10:13:04 +0000

    binary

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -19,6 +19,7 @@ int binary_search(int *array, size_t size, int value)
 	while (left <= right)
 	{
 		int mid = left + (right - left) / 2;
+
 		printf("Searching in array: ");
 		for (i = left; i <= right; i++)
 		{
