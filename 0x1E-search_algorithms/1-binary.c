898ec5301d159c3c0fb6767249e1bf3055e6098f
Obi <williamobi818@gmail.com>
2023-10-10 10:00:25 +0000

    push binary

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -13,6 +13,11 @@ int binary_search(int *array, size_t size, int value)
 	int right = size - 1;
 	int i;
 
+	if (array == NULL)
+	{
+		return (-1);
+	}
+
 	while (left <= right)
 	{
 		int mid = left + (right - left) / 2;
