b13225550e08138b5d101bafc753b55b4de0029b
Obi <williamobi818@gmail.com>
2023-10-10 09:39:45 +0000

    more update

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -8,14 +8,14 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-	int i;
+	size_t i;
 
 	for (i = 0; i < size; i++)
 	{
-		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 		{
-			return (array[i]);
+			return (i);
 		}
 	}
 	return (-1);
