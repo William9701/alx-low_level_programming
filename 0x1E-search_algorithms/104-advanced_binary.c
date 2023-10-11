726e58bcebb43759c20abb9fbe36444707620b8a
Obi <williamobi818@gmail.com>
2023-10-11 20:12:45 +0000

    hjyjk

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -1,46 +1,43 @@
 #include "search_algos.h"
 
-
 /**
  * binary_search_recursion - Recursively y
  *
  * @array: Pointer to the first element of the array to search in
- * @left: Index of the first element in current search space
- * @right: Index of the last element in current search space
+ * @low: Index of the first element in current search space
+ * @high: Index of the last element in current search space
  * @value: Value to search for
  *
  * Return: The index where value is located, or -1 on failure
  */
-int binary_search_recursion(int *array, int value, 
-                             size_t low, size_t high) 
- { 
-         size_t mid, i; 
-  
-         if (!array) 
-                 return (-1); 
-  
-         mid = (low + high) / 2; 
-         printf("Searching in array: "); 
-         for (i = low; i <= high; i++) 
-                 printf("%i%s", array[i], i == high ? "\n" : ", "); 
-  
-         if (array[low] == value) 
-                 return ((int)low); 
-  
-         if (array[low] != array[high]) 
-         { 
-                 if (array[mid] < value) 
-                         return (binary_search_recursion(array, value, 
-                                                         mid + 1, high)); 
-                 if (array[mid] >= value) 
-                         return (binary_search_recursion(array, value, 
-                                                         low, mid)); 
-         } 
-  
-         return (-1); 
- } 
+int binary_search_recursion(int *array, int value,
+				size_t low, size_t high)
+{
+	size_t mid, i;
 
+	if (!array)
+		return (-1);
 
+	mid = (low + high) / 2;
+	printf("Searching in array: ");
+	for (i = low; i <= high; i++)
+		printf("%i%s", array[i], i == high ? "\n" : ", ");
+
+	if (array[low] == value)
+		return ((int)low);
+
+	if (array[low] != array[high])
+	{
+		if (array[mid] < value)
+			return (binary_search_recursion(array, value,
+							mid + 1, high));
+		if (array[mid] >= value)
+			return (binary_search_recursion(array, value,
+							low, mid));
+	}
+
+	return (-1);
+}
 
 /**
  * advanced_binary - Searches for a vh
@@ -51,14 +48,14 @@ int binary_search_recursion(int *array, int value,
  *
  * Return: The index where value is located, or -1 on failure
  */
-int advanced_binary(int *array, size_t size, int value) 
- { 
-         size_t low = 0; 
-         size_t high = size - 1; 
-  
-         if (!array) 
-                 return (-1); 
-  
-         return (binary_search_recursion(array, value, low, high)); 
- }
+int advanced_binary(int *array, size_t size, int value)
+{
+	size_t low = 0;
+	size_t high = size - 1;
+
+	if (!array)
+		return (-1);
+
+	return (binary_search_recursion(array, value, low, high));
+}
 
