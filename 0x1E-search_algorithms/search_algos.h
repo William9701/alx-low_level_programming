be21828d0021f40ffe626360aaf2505a2c1ad7f5
Obi <williamobi818@gmail.com>
2023-10-11 20:48:33 +0000

    dones

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -27,6 +27,7 @@ void print_array(int *array, size_t left, size_t right);
 int binary_search_recursive(int *array, size_t left, size_t right, int value);
 int advanced_binary(int *array, size_t size, int value);
 listint_t *jump_list(listint_t *list, size_t size, int value);
+skiplist_t *linear_skip(skiplist_t *list, int value);
 
 typedef struct skiplist_s
 {
